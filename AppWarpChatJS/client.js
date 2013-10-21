104f5b8c2b7c26b055028028eb13150d61c53974
Manav Kataria <manavkataria.iitb@gmail.com>
2013-10-20 23:34:05 -0700

    Copying Ballons.IO from https://github.com/gravityonmars/Balloons.IO

--- AppWarpChatJS/client.js
+++ AppWarpChatJS/client.js
@@ -78,8 +78,9 @@ function onUnsubscribeRoomDone(room)
 }
 
 function onChatReceived(chat)
-{
-    $("#chat").html("<dt class='text-danger'>"+chat.getSender() + "</dt><dd class='text-primary'>" + chat.getChat() + "</dd>" + $("#chat").html());
+{	
+	imgsrc = 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/c5.5.65.65/s50x50/282965_468653659847682_8020574_t.jpg';
+    $("#chat").html($("#chat").html() + "<dd><img class='profilepic' src='"+imgsrc+"' alt=''><div class='chatTextBlock'><span class='text-danger'>" + chat.getSender() + "</span><span class='text-primary'>" + chat.getChat() + "</span></div></dd>" );
 }
 
 function joinRoom(id)
@@ -109,6 +110,8 @@ function setListeners(_warpclient){
 
 $(document).ready(function(){
     $("#roomsRow").hide();
+
+    //TODO: Trigger on enter key.
     $("#nameBtn").click(function(){
         
 		if ($("#nameText").val() != "")
--- README.md
+++ README.md
@@ -1,27 +1,26 @@
 antakshari-web
 ==============
-
 Antakshari Server &amp; Client | Facebook App | Meteor / Node.js
 
 
 Lucid Landing
 =============
-
 Version 0.1
 lucid-landing.meteor.com
 
 
 Movie Library
 =============
-
 Version 0.1
+
+Chat Server/Client (Ballons.IO) 
+===============================
+Source: https://github.com/gravityonmars/Balloons.IO
 
 
 Chat Client (AppWarp JS) 
 =============
-
 Version 0.1
-
 
 Facebook App
 ============
